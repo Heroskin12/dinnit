fed0363d35867cd0eb55c4b09daceab3ffbbc479
Heroskin12 <jamiefurlong16@gmail.com>
2024-09-28 22:42:05 +0100

    Created project file system. Changed styles to NativeWind

--- dinnit/app/_layout.jsx
+++ dinnit/app/_layout.jsx
@@ -1,14 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Satisfy_400Regular, useFonts } from "@expo-google-fonts/satisfy";
+import React, { useEffect } from "react";
+// Component that displays the current child route.
+import { Slot, SplashScreen, Stack } from 'expo-router';
 
+SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
+    const [fontsLoaded, error] = useFonts({
+        Satisfy_400Regular
+    });
+
+    console.log(fontsLoaded);
+
+    useEffect(() => {
+        if(error) throw error;
+        if(fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error])
+
+    if(!fontsLoaded && !error) return null;
+
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false}} />
+    </Stack>
   );
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({});
---
+++ dinnit/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF4C4C",
+        secondary: {
+          DEFAULT: "#FF9C01",
+          100: "#FF9001",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        satisfy: ["Satisfy_400Regular", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
