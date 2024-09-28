c3b18877a6d6ff9a883234a44de95c3469145fdf
Heroskin12 <jamiefurlong16@gmail.com>
2024-09-28 16:19:42 +0100

    Changed directory structure

---
+++ dinnit/app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
